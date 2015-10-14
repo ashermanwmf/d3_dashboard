a9cf52dd96dafdaa90ef0b518e74abe018350ca1
ashermanwmf <asherman@wikimedia.org>
2015-10-13 21:36:12 -0400

    made a mysql database for the python app

--- hello.py
+++ hello.py
@@ -1,4 +1,25 @@
 from flask import Flask, Blueprint, send_file, jsonify
+from flaskext.mysql import MySQL
+
+mysql = MySQL()
+app = Flask(__name__)
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
+app.config['MYSQL_DATABASE_DB'] = 'EmpData'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+mysql.init_app(app)
+
+@app.route("/Authenticate")
+def Authenticate():
+    username = request.args.get('Username')
+    password = request.args.get('Password')
+    cursor = mysql.connect().cursor()
+    cursor.execute("SELECT * from User where Username='" + username + "' and Password='" + password + "'")
+    data = cursor.fetchone()
+    if data is None:
+        return "Username or Password is wrong"
+    else:
+        return "Logged in successfully"
 
 client_app = Blueprint('client_app', __name__,
                     static_url_path='/dashboard',
@@ -19,8 +40,6 @@ def data():
         Query the DB for 'data' and return data as JSON
     '''
     return jsonify(x);
-
-
 
 app = Flask(__name__)
 app.register_blueprint(client_app)
