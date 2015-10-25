d765c10921d7b49149ef2b4238fa4c6dc87d174a
ashermanwmf <asherman@wikimedia.org>
2015-10-24 21:08:03 -0400

    adding updates

--- dashboard/includes/js/functions.js
+++ dashboard/includes/js/functions.js
@@ -1,25 +1,36 @@
 function getdata() {
-var my_data = '';
-    
-$.getJSON('dashboard/includes/data/data.json', function(data) {
-    console.log(data);
-    my_data = JSON.parse(data); 
+$('#button').on('click', function() {
+                $.ajax({
+                    url: 'data',
+                    type: 'POST',
+                    success: function(data) {
+                        d3_test(data);
+                        $('#demo').html(data.age);
+                    }
+                });
+                $.ajax({ 
+                    url: '/dashboard/includes/data/october',
+                    type: 'POST',
+                    success: function(data) {
+                        d3_test(data);
+                        $('#demo2').html(data[1].FIELD2);
+                    }
+                }); 
 });
-    
-    $.ajax({
-        url: 'dashboard/includes/data/data.json',
-        type: 'GET',
-        success: function(data) {
-            console.log(data);   
-        }
-    });
 
-console.log(my_data);
+function d3_test(data) {
 
-return document.getElementById("demo").innerHTML = 
-        my_data.name + "<br>" +
-        my_data.street + "<br>" +
-        my_data.phone;
-    
+    console.log(data)
+
 }
 
+     $('#searchterm').on('click', function() {
+                $.ajax({ 
+                    url: '/dashboard/includes/data/september',
+                    type: 'POST',
+                    success: function(data) {
+                        d3_test(data);
+                        $('#demo3').html(data[1].FIELD2);
+                    }
+                });  
+            });
--- hello.py
+++ hello.py
@@ -1,6 +1,8 @@
-from flask import Flask, Blueprint, send_file, jsonify
+import os
+from flask import Flask, Blueprint, send_file, jsonify, Response
 from flaskext.mysql import MySQL
 
+#for mysql server
 mysql = MySQL()
 app = Flask(__name__)
 app.config['MYSQL_DATABASE_USER'] = 'root'
@@ -9,6 +11,7 @@ app.config['MYSQL_DATABASE_DB'] = 'EmpData'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
 
+#for mysql server
 @app.route("/Authenticate")
 def Authenticate():
     username = request.args.get('Username')
@@ -21,11 +24,13 @@ def Authenticate():
     else:
         return "Logged in successfully"
 
+# for acessing the right pasth to index.html in dashboards/
 client_app = Blueprint('client_app', __name__,
                     static_url_path='/dashboard',
                     static_folder='dashboard',
                     template_folder='dashboard')
 
+# show the index.html
 @client_app.route('/', methods=['GET', 'POST'])
 def home():
     return send_file('dashboard/index.html')
@@ -34,6 +39,7 @@ def home():
 x = { "name"    : "test"
     , "age"     : "12"}
 
+# sample preform json
 @client_app.route('/data', methods=['POST'])
 def data():
     '''
@@ -41,9 +47,23 @@ def data():
     '''
     return jsonify(x);
 
+# sample preform json
+@client_app.route('/dashboard/includes/data/<file>', methods=['POST'])
+def october(file = None):
+    '''
+        Query the DB for 'data' and return data as JSON
+    '''
+    fileName = os.path.abspath('./dashboard/includes/data/' + file + '.json')
+    with open(fileName, 'r') as f:
+        ret = f.read()
+    f.close()
+    return Response(ret, mimetype="application/json")
+
+# runs the server
 app = Flask(__name__)
 app.register_blueprint(client_app)
  
+# runs the server
 if __name__ == '__main__':
     app.run(debug = True, use_reloader = True)
   
