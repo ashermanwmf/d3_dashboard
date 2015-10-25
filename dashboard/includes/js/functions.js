1866d79c687a4ac621d3b88c1db2a611e92d5817
ashermanwmf <asherman@wikimedia.org>
2015-10-25 16:48:53 -0400

    works, just need to add svg elements now

--- dashboard/includes/js/functions.js
+++ dashboard/includes/js/functions.js
@@ -1,36 +1,40 @@
-function getdata() {
-$('#button').on('click', function() {
-                $.ajax({
-                    url: 'data',
-                    type: 'POST',
-                    success: function(data) {
-                        d3_test(data);
-                        $('#demo').html(data.age);
-                    }
-                });
-                $.ajax({ 
-                    url: '/dashboard/includes/data/october',
-                    type: 'POST',
-                    success: function(data) {
-                        d3_test(data);
-                        $('#demo2').html(data[1].FIELD2);
-                    }
-                }); 
-});
-
 function d3_test(data) {
 
     console.log(data)
 
-}
+};
 
-     $('#searchterm').on('click', function() {
-                $.ajax({ 
-                    url: '/dashboard/includes/data/september',
-                    type: 'POST',
-                    success: function(data) {
-                        d3_test(data);
-                        $('#demo3').html(data[1].FIELD2);
-                    }
-                });  
-            });
+function getdata() {
+$('#button').on('click', function() {
+        $.ajax({
+            url: 'data',
+            type: 'POST',
+            success: function(data) {
+                d3_test(data);
+                $('#demo').html(data.age);
+        }
+    });
+        $.ajax({ 
+            url: '/dashboard/includes/data/october',
+            type: 'POST',
+            success: function(data) {
+                d3_test(data); 
+                $('#demo2').html(data[1].FIELD2);
+        }
+    });
+        $.ajax({ 
+            url: '/dashboard/includes/data/september',
+            type: 'POST',
+            success: function(data) {
+                d3_test(data);
+                $('#demo3').html(data[1].FIELD2);
+        }
+    });  
+});
+};
+
+/* 
+$('#searchterm').on('click', function() {
+        
+});
+*/
