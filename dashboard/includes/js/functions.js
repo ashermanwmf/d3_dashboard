1bd3198ae4960ff5b4c76d9ae2bf6229141fab7f
ashermanwmf <asherman@wikimedia.org>
2015-10-25 22:10:54 -0400

    adding search features

--- dashboard/includes/js/functions.js
+++ dashboard/includes/js/functions.js
@@ -1,10 +1,5 @@
-function d3_test(data) {
-
-    console.log(data)
-
-};
-
 function getdata() {
+/* test to see if I can read json to my html 
 $('#button').on('click', function() {
         $.ajax({
             url: 'data',
@@ -22,19 +17,49 @@ $('#button').on('click', function() {
                 $('#demo2').html(data[1].FIELD2);
         }
     });
-        $.ajax({ 
+       
+});
+*/
+/* trying to get a search box to work for my data (starting simple - adding drop down list of choices while searching next) */
+$('#search').on('click', function() {
+    $.ajax({ 
+            url: '/dashboard/includes/data/october',
+            type: 'POST',
+            success: function(data) {
+                d3_test(data); 
+                /* Search for an item in the  */
+                var search_field = $('#searchterm').val();
+                for (i = 0; i < data.length; i++) {
+                    if (search_field == data[i].FIELD1) {
+                       $('#demo2').append("This exists");
+                    } else {
+                        $('#demo2').html("Try again."); 
+                    }
+                }
+            }
+    });
+      $.ajax({ 
             url: '/dashboard/includes/data/september',
             type: 'POST',
             success: function(data) {
                 d3_test(data);
-                $('#demo3').html(data[1].FIELD2);
-        }
-    });  
+                var search_field = $('#searchterm').val();
+                for (i = 0; i < data.length; i++) {
+                    if (search_field == data[i].FIELD1) {
+                       $('#demo2').append("This exists");
+                    } else {
+                        $('#demo2').html("Try again."); 
+                    }
+                }
+            }
+    }); 
+            
 });
-};
 
-/* 
-$('#searchterm').on('click', function() {
-        
-});
-*/
+}
+    
+function d3_test(data) {
+
+    console.log(data)
+
+};
