5d3bf913a03337957177864db0c2f2d0c24953d7
ashermanwmf <asherman@wikimedia.org>
2015-10-26 09:24:41 -0400

    made two search bars for choosing stories to compare

--- dashboard/includes/js/functions.js
+++ dashboard/includes/js/functions.js
@@ -1,61 +1,42 @@
 function getdata() {
-/* test to see if I can read json to my html 
-$('#button').on('click', function() {
-        $.ajax({
-            url: 'data',
-            type: 'POST',
-            success: function(data) {
-                d3_test(data);
-                $('#demo').html(data.age);
-        }
-    });
-        $.ajax({ 
-            url: '/dashboard/includes/data/october',
-            type: 'POST',
-            success: function(data) {
-                d3_test(data); 
-                $('#demo2').html(data[1].FIELD2);
-        }
-    });
-       
-});
-*/
-/* trying to get a search box to work for my data (starting simple - adding drop down list of choices while searching next) */
-$('#search').on('click', function() {
-    $.ajax({ 
-            url: '/dashboard/includes/data/october',
-            type: 'POST',
-            success: function(data) {
-                d3_test(data); 
-                /* Search for an item in the  */
-                var search_field = $('#searchterm').val();
-                for (i = 0; i < data.length; i++) {
-                    if (search_field == data[i].FIELD1) {
-                       $('#demo2').append("This exists");
-                    } else {
-                        $('#demo2').html("Try again."); 
-                    }
-                }
-            }
-    });
+/* Only need to search in september (can't do it from one search bar yet) */
+$('#search_1').on('click', function() {
       $.ajax({ 
-            url: '/dashboard/includes/data/september',
-            type: 'POST',
-            success: function(data) {
-                d3_test(data);
-                var search_field = $('#searchterm').val();
-                for (i = 0; i < data.length; i++) {
-                    if (search_field == data[i].FIELD1) {
-                       $('#demo2').append("This exists");
-                    } else {
-                        $('#demo2').html("Try again."); 
-                    }
-                }
-            }
-    }); 
+          url: '/dashboard/includes/data/september',
+          type: 'POST',
+          success: function(data) {
+              d3_test(data);
+              var search_field = $('#searchterm_1').val();
+              for (i = 0; i < data.length; i++) {
+                  if (search_field == data[i].FIELD1) {
+                      $('#choice_1').append('Choice 1: ');
+                      $('#choice_1').append(data[i].FIELD1);
+                  }
+              }
+          }
+      }); 
             
 });
 
+$('#search_2').on('click', function() {
+    /* THIS WORKS: basic search for json september, need to make it show options bellow in a list. */
+      $.ajax({ 
+          url: '/dashboard/includes/data/september',
+          type: 'POST',
+          success: function(data) {
+              d3_test(data);
+              var search_field = $('#searchterm_2').val();
+              for (i = 0; i < data.length; i++) {
+                  if (search_field == data[i].FIELD1) {
+                      $('#choice_2').append('Choice 2: ');
+                      $('#choice_2').append(data[i].FIELD1);
+                  }
+              }
+          }
+      }); 
+            
+});
+    
 }
     
 function d3_test(data) {
