d8aef9c99a9bdaef2249f3331e720c978069050e
ashermanwmf <asherman@wikimedia.org>
2015-10-26 20:17:12 -0400

    adding attempt at live search with jquery-ui (400 error with jquery mini)

--- dashboard/includes/js/functions.js
+++ dashboard/includes/js/functions.js
@@ -1,3 +1,6 @@
+var Story_One = '';
+var Story_Two = '';
+
 function getdata() {
 /* Only need to search in september (can't do it from one search bar yet) */
 $('#search_1').on('click', function() {
@@ -5,11 +8,12 @@ $('#search_1').on('click', function() {
           url: '/dashboard/includes/data/september',
           type: 'POST',
           success: function(data) {
-              d3_test(data);
+              d3_test(data); 
               var search_field = $('#searchterm_1').val();
               for (i = 0; i < data.length; i++) {
                   if (search_field == data[i].FIELD1) {
-                      $('#choice_1').append('Choice 1: ');
+                      var Story_One = data[i].FIELD1;
+                      $('#choice_1').html('Choice 1: ');
                       $('#choice_1').append(data[i].FIELD1);
                   }
               }
@@ -28,15 +32,65 @@ $('#search_2').on('click', function() {
               var search_field = $('#searchterm_2').val();
               for (i = 0; i < data.length; i++) {
                   if (search_field == data[i].FIELD1) {
-                      $('#choice_2').append('Choice 2: ');
+                      var Story_Two = data[i].FIELD1;
+                      $('#choice_2').html('Choice 2: ');
                       $('#choice_2').append(data[i].FIELD1);
                   }
-              }
+              }   
           }
       }); 
             
 });
+    $.ajax({
+            type: "POST",
+            contentType: "/dashboard/includes/data/september; charset=utf-8",
+            url: 'url to your web servise',
+            dataType: 'json',
+            async: true,
+            data: "{}", 
+            success: function (data) {
+               var pos_data = data;
+               div_name = "div.svg";
+               draw_svg(div_name, pos_data);
+            },
+            error: function (result) {
+            }
+    });
     
+/* trying to get live search to work */
+ 
+    function log( message ) {
+                  $( "<div>" ).text( message ).prependTo( "#log" );
+                  $( "#log" ).scrollTop( 0 );
+                }
+                $( "#search" ).autocomplete({
+                  source: function( request, response ) {
+                    $.ajax({
+                      url: '/dashboard/includes/data/september.json',
+                      dataType: "jsonp",
+                      data: {
+                        q: request.term
+                      },
+                      success: function( data ) {
+                        response( data );
+                      }
+                    });
+                  },
+                  minLength: 3,
+                  select: function( event, ui ) {
+                    log( ui.item ?
+                      "Selected: " + ui.item.label :
+                      "Nothing selected, input was " + this.value);
+                  },
+                  open: function() {
+                    $( this ).removeClass( "ui-corner-all" ).addClass( "ui-corner-top" );
+                  },
+                  close: function() {
+                    $( this ).removeClass( "ui-corner-top" ).addClass( "ui-corner-all" );
+                  }
+                });
+    
+/* trying to get live search to work */
 }
     
 function d3_test(data) {
@@ -44,3 +98,6 @@ function d3_test(data) {
     console.log(data)
 
 };
+
+function draw_svg(reference, pos_data){
+}
