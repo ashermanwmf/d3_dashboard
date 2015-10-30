70cc7d5d8c269d90aee18c10059447533b8d16cf
ashermanwmf <asherman@wikimedia.org>
2015-10-30 08:50:10 -0400

    working live search

--- dashboard/includes/js/functions.js
+++ dashboard/includes/js/functions.js
@@ -2,102 +2,123 @@ var Story_One = '';
 var Story_Two = '';
 
 function getdata() {
-/* Only need to search in september (can't do it from one search bar yet) */
-$('#search_1').on('click', function() {
-      $.ajax({ 
-          url: '/dashboard/includes/data/september',
-          type: 'POST',
-          success: function(data) {
-              d3_test(data); 
-              var search_field = $('#searchterm_1').val();
-              for (i = 0; i < data.length; i++) {
-                  if (search_field == data[i].FIELD1) {
-                      var Story_One = data[i].FIELD1;
-                      $('#choice_1').html('Choice 1: ');
-                      $('#choice_1').append(data[i].FIELD1);
-                  }
-              }
-          }
-      }); 
-            
-});
-
-$('#search_2').on('click', function() {
-    /* THIS WORKS: basic search for json september, need to make it show options bellow in a list. */
-      $.ajax({ 
-          url: '/dashboard/includes/data/september',
-          type: 'POST',
-          success: function(data) {
-              d3_test(data);
-              var search_field = $('#searchterm_2').val();
-              for (i = 0; i < data.length; i++) {
-                  if (search_field == data[i].FIELD1) {
-                      var Story_Two = data[i].FIELD1;
-                      $('#choice_2').html('Choice 2: ');
-                      $('#choice_2').append(data[i].FIELD1);
-                  }
-              }   
-          }
-      }); 
-            
-});
-    $.ajax({
-            type: "POST",
-            contentType: "/dashboard/includes/data/september; charset=utf-8",
-            url: 'url to your web servise',
-            dataType: 'json',
-            async: true,
-            data: "{}", 
-            success: function (data) {
-               var pos_data = data;
-               div_name = "div.svg";
-               draw_svg(div_name, pos_data);
-            },
-            error: function (result) {
-            }
-    });
-    
-/* trying to get live search to work */
- 
-    function log( message ) {
-                  $( "<div>" ).text( message ).prependTo( "#log" );
-                  $( "#log" ).scrollTop( 0 );
+    /* Only need to search in september (can't do it from one search bar yet) */
+    $('#search_1').on('click', function() {
+        $.ajax({
+            url: '/dashboard/includes/data/september',
+            type: 'POST',
+            success: function(data) {
+                d3_test(data);
+                var search_field = $('#searchterm_1').val();
+                for (i = 0; i < data.length; i++) {
+                    if (search_field == data[i].FIELD1) {
+                        var Story_One = data[i].FIELD1;
+                        $('#choice_1').html('Choice 1: ');
+                        $('#choice_1').append(data[i].FIELD1);
+                    }
                 }
-                $( "#search" ).autocomplete({
-                  source: function( request, response ) {
-                    $.ajax({
-                      url: '/dashboard/includes/data/september.json',
-                      dataType: "jsonp",
-                      data: {
-                        q: request.term
-                      },
-                      success: function( data ) {
-                        response( data );
-                      }
-                    });
-                  },
-                  minLength: 3,
-                  select: function( event, ui ) {
-                    log( ui.item ?
-                      "Selected: " + ui.item.label :
-                      "Nothing selected, input was " + this.value);
-                  },
-                  open: function() {
-                    $( this ).removeClass( "ui-corner-all" ).addClass( "ui-corner-top" );
-                  },
-                  close: function() {
-                    $( this ).removeClass( "ui-corner-top" ).addClass( "ui-corner-all" );
-                  }
-                });
-    
-/* trying to get live search to work */
+            }
+        });
+
+    });
+
+    $('#search_2').on('click', function() {
+        /* THIS WORKS: basic search for json september, need to make it show options bellow in a list. */
+        $.ajax({
+            url: '/dashboard/includes/data/september',
+            type: 'POST',
+            success: function(data) {
+                d3_test(data);
+                var search_field = $('#searchterm_2').val();
+                for (i = 0; i < data.length; i++) {
+                    if (search_field == data[i].FIELD1) {
+                        var Story_Two = data[i].FIELD1;
+                        $('#choice_2').html('Choice 2: ');
+                        $('#choice_2').append(data[i].FIELD1);
+                    }
+                }
+            }
+        });
+
+    });
+    $.ajax({
+        type: "POST",
+        contentType: "/dashboard/includes/data/september; charset=utf-8",
+        dataType: 'json',
+        async: true,
+        data: "{}",
+        success: function(data) {
+            var pos_data = data;
+            div_name = "div.svg";
+            draw_svg(div_name, pos_data);
+        },
+        error: function(result) {}
+    });
+
+    /* trying to get live search to work */
+
+    function log(message) {
+        $("<div>").text(message).prependTo("#log");
+        $("#log").scrollTop(0);
+    }
+    $("#search").autocomplete({
+        source: function(request, response) {
+            $.ajax({
+                type: "POST",
+                url: "/dashboard/includes/data/september",
+                success: function(data) {
+                    console.log(data);
+					data.sort(function(a, b) {
+						return (a.FIELD2 < b.FIELD2) ? 1 : ((b.FIELD2 < a.FIELD2) ? -1 : 0);
+					});
+					ret = [];
+					for(i = 0; i < data.length; i++) {
+						if (data[i].FIELD1.indexOf(request.term) === 0) {
+							console.log(data[i]);
+							ret.push(data[i].FIELD1 +" " + data[i].FIELD2);
+						}
+					}
+					console.log(ret);
+                    response(ret);
+                }
+            });
+        },
+        minLength: 3,
+        select: function(event, ui) {
+            log(ui.item ?
+				"Selected: " + ui.item.label :
+                "Nothing selected, input was " + this.value);
+        },
+        open: function() {
+            $(this).removeClass("ui-corner-all").addClass("ui-corner-top");
+        },
+        close: function() {
+            $(this).removeClass("ui-corner-top").addClass("ui-corner-all");
+        }
+    });
+
+    /* trying to get live search to work */
 }
-    
+
 function d3_test(data) {
 
     console.log(data)
 
 };
 
-function draw_svg(reference, pos_data){
-}
+function draw_svg(reference, pos_data) {}
+
+/* $.ajax({
+                      type: "POST",
+					  contentType: "/dashboard/includes/data/september; charset=utf-8",
+					  dataType: 'json',
+					  async: true,
+					  data: "{}", 
+					  success: function (data) {
+                        q: request.term
+                      },
+                      success: function( data ) {
+                          console.log(data);
+                          response( data );
+                      }
+                    }); */
